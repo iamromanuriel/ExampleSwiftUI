a02d77dbc7a2d972e1ec8081ff57e2d47ab9d4d1
iamromanuriel <romanufranciscomartinez@gmail.com>
2025-03-05 17:50:21 -0600

    Initial Commit

---
+++ ExampleSwiftUI/ExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUIApp.swift
+//  ExampleSwiftUI
+//
+//  Created by Roman Uriel on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExampleSwiftUITests/ExampleSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUITests.swift
+//  ExampleSwiftUITests
+//
+//  Created by Roman Uriel on 05/03/25.
+//
+
+import Testing
+@testable import ExampleSwiftUI
+
+struct ExampleSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
